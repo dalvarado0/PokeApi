38bfd731f1ab579837d81337bfca7cca54ca319d
David Alvarado <dalvarado0@ucol.mx>
2019-03-24 23:35:55 -0600

    La imagen se repite en cada tarjeta de los pokemones

--- src/components/PokeInfoContainer/PokemonData/Image.js
+++ src/components/PokeInfoContainer/PokemonData/Image.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 
-var PokeImage;
-
 const GetPokeImage = ({ data }) => {
     axios.get(data)
         .then(res => {
-            PokeImage = res.data.sprites.front_default;
-            console.log(PokeImage);
+            const PokeImage = res.data.sprites.front_default;
+            console.log("Pokemon name: "+ res.data.name);
+            console.log("Image Pokemon URL: " +PokeImage);
+            return(
+                <img src={PokeImage} alt="Pokemon" />
+            );
         });
-        return(
-            <img src={PokeImage} alt="Pokemon" />
-        );
 }
 
 const Image = ({data})=>{
